aef9c587e0c0e98676a8bb9971612314af96c4de
ganesh714 <evvganesh1@gmail.com>
2026-05-19 20:56:41 +0530

    feat(controller): wrap endpoints with ResponseEntity and use appropriate HTTP statuses

--- src/main/java/com/virax/restapi/contact_api/controller/ContactController.java
+++ src/main/java/com/virax/restapi/contact_api/controller/ContactController.java
@@ -3,6 +3,8 @@ package com.virax.restapi.contact_api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,42 +25,45 @@ public class ContactController {
 	ContactOperations co;
 	
 	@PostMapping("add/contact")
-	public boolean addContact(@RequestBody ContactDto contactDto) {
-		return co.addContact(contactDto);
+	public ResponseEntity<ContactDto> addContact(@RequestBody ContactDto contactDto) {
+		return new ResponseEntity<>(co.addContact(contactDto),HttpStatus.CREATED);
 	}
 	
 	@PostMapping("add/multi/contact")
-	public List<ContactDto> addMultipleContact(@RequestBody List<ContactDto> ip) {
-		return co.addMultipleContacts(ip);
+	public ResponseEntity<List<ContactDto>> addMultipleContact(@RequestBody List<ContactDto> ip) {
+		return new ResponseEntity<>(co.addMultipleContacts(ip),HttpStatus.MULTI_STATUS);
 	}
 	
 	@GetMapping("get/contact/{mobileNumber}")
-	public ContactDto getContact(@PathVariable String mobileNumber) {
-		return co.getContactByMobileNumber(mobileNumber);
+	public ResponseEntity<ContactDto> getContact(@PathVariable String mobileNumber) {
+		return new ResponseEntity<>(co.getContactByMobileNumber(mobileNumber),HttpStatus.OK);
 	}
 	
 	@GetMapping("get/all/contacts")
-	public List<ContactDto> gatAllContacts(){
-		return co.getAllContacts();
+	public ResponseEntity<List<ContactDto>> gatAllContacts(){
+		return new ResponseEntity<>(co.getAllContacts(),HttpStatus.OK);
 	}
 	
 	@PutMapping("/update/name/{mobileNumber}")
-	public void updateName(@PathVariable String mobileNumber, @RequestBody String name) {
+	public ResponseEntity<String> updateName(@PathVariable String mobileNumber, @RequestBody String name) {
 		co.updateContactName(mobileNumber, name);
+		return new ResponseEntity<>("Name updated successfully!",HttpStatus.OK);
 	}
 	
 	@PutMapping("/updateByNumber/{mobileNumber}")
-	public void updateContact(@PathVariable String mobileNumber, @RequestBody ContactDto c) {
+	public ResponseEntity<String> updateContact(@PathVariable String mobileNumber, @RequestBody ContactDto c) {
 		co.updateContact(mobileNumber, c);
+		return new ResponseEntity<>("Contact updated successfully!",HttpStatus.OK);
 	}
 	
 	@DeleteMapping("/deleteByNumber/{mobileNumber}")
-	public void deleteContact(@PathVariable String mobileNumber) {
+	public ResponseEntity<String> deleteContact(@PathVariable String mobileNumber) {
 		co.deleteContactByMobileNumber(mobileNumber);
+		return new ResponseEntity<>("Contact deleted successfully", HttpStatus.OK);
 	}
 	
 	@GetMapping("/search/{key}")
-	public List<ContactDto> search(@PathVariable String key) {
-		return co.searchContacts(key);
+	public ResponseEntity<List<ContactDto>> search(@PathVariable String key) {
+		return new ResponseEntity<>(co.searchContacts(key),HttpStatus.OK);
 	}
 }
