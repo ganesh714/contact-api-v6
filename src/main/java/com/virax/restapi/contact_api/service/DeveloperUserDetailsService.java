2bfb10aa39f07852614fe0534e8459a347b415cd
ganesh714 <evvganesh1@gmail.com>
2026-05-31 23:28:09 +0530

    create developer UserDetailsService

---
+++ src/main/java/com/virax/restapi/contact_api/service/DeveloperUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.virax.restapi.contact_api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.virax.restapi.contact_api.model.Developer;
+import com.virax.restapi.contact_api.model.DeveloperUserDetails;
+import com.virax.restapi.contact_api.repository.DeveloperRespository;
+
+public class DeveloperUserDetailsService implements UserDetailsService{
+	
+	@Autowired
+	DeveloperRespository developerRespository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Developer dev = developerRespository.findByUserName(username);
+		if (dev == null) {
+			throw new UsernameNotFoundException(username);
+		}
+		return new DeveloperUserDetails(dev);
+	}
+
+}
