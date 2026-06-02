02323c0a028ff6f991485262fdfecb2c1f5f0b69
ganesh714 <evvganesh1@gmail.com>
2026-06-02 16:32:36 +0530

    updated dev user details serv

--- src/main/java/com/virax/restapi/contact_api/service/DeveloperUserDetailsService.java
+++ src/main/java/com/virax/restapi/contact_api/service/DeveloperUserDetailsService.java
@@ -4,14 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.virax.restapi.contact_api.model.Developer;
 import com.virax.restapi.contact_api.model.DeveloperUserDetails;
 import com.virax.restapi.contact_api.repository.DeveloperRespository;
 
-import io.swagger.v3.oas.annotations.servers.Server;
 
-@Server
+@Service
 public class DeveloperUserDetailsService implements UserDetailsService{
 	
 	@Autowired
