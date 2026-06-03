6f3fe559c2b2ba9d1ad5313df99219e7a3545dda
ganesh714 <evvganesh1@gmail.com>
2026-06-03 21:41:58 +0530

    mtm updated dev role

--- src/main/java/com/virax/restapi/contact_api/model/Developer.java
+++ src/main/java/com/virax/restapi/contact_api/model/Developer.java
@@ -1,7 +1,13 @@
 package com.virax.restapi.contact_api.model;
 
+import java.util.Set;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,5 +21,13 @@ public class Developer {
 	@Id
 	private int id;
 	private String userName;
-	private String passward;
+	private String password;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(
+		name = "developer_roles",
+		joinColumns = @JoinColumn(name = "developer_id"),
+		inverseJoinColumns = @JoinColumn(name = "role_id")
+	)
+	private Set<Role> roles;
 }
